40cf7393bb9723d12b1726c7a0448c2f4db8fc93
Doug Hooper <hooprds@gmail.com>
2023-03-09 01:47:02 -0500

    sdv

--- Python 2.2/08-Ins_ReadCSV/Unsolved/read_csv.py
+++ Python 2.2/08-Ins_ReadCSV/Unsolved/read_csv.py
@@ -3,43 +3,21 @@ import os # allows for operating system / file handling functions
 
 # import module for csv files
 import csv
+# import csv # functions to intraverse through csv files, go through rows as a series of lists
 
 # general file path to our contacts.csv - 
-# ../Resources/contacts.csv
+#../Resources/contacts.csv
 
-# instead, we can use os.path.join to form a path to the csv file
-csvFilePath = os.path.join("..", "Resources", "contacts.csv")
+# instead, we use os.path.join to form a path to the csv file
+csvFilePath = os.path.join("..", "..", "Resources", "contacts.csv")
 
 # use the with open() functino to open the csvFilePath into an object
 with open(csvFilePath) as csvFile:
 
-    # csv module .reader() funtion specifies the delimiter and object name for the reader in the open()
-    # reader in the open() funtion
-    csvReader = csv.reader(csvFile, delimiter=",")
+#csv module .reader() function specifies the delimiter and object name for the reader in the open() function
+        csvReader = csv.reader(csvFile, delimiter=",")
 
-    # the csvReader has the info in the file, split into rows of lists
-    # that correspond to each row of data in the file
-    
-    #if the first row has header data, use next() to pass over the row
-    header = next(csvReader) # skips the first row and stores into a variable
-    print(header) 
-    print("--------")
-
-    # read each row of data and display each row of data
-    #for row in csvReader:
-        #print(row)
-
-print("----------------")
-# print out first names (index 0) of each row
-for row in csvReader:
-        print(row[0])
-        #for item in range(len(row))
-        #   print(row[item])
-
-# in order to write to a file, use with open(filepath, "w")
-
-#outputFilepath = "../Resources/output.csv"
-
-outputFilePath = os.path.join("..", "Resources", "output.csv")
-
-with open(outputFilePath, "w") as csvOutput:
+        # the csvReader has the info in the file, split into rows of lists
+        #that correspond to each row of data in the file
+        for row in csvReader:
+                print(row)
