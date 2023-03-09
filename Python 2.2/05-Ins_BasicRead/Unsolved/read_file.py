8ca69166baf782ff2f712fff80064db236bdc2ed
Doug Hooper <hooprds@gmail.com>
2023-03-09 01:02:59 -0500

    ui,l

--- Python 2.2/05-Ins_BasicRead/Unsolved/read_file.py
+++ Python 2.2/05-Ins_BasicRead/Unsolved/read_file.py
@@ -2,3 +2,11 @@
 filepath = "../Resources/input.txt"
 
 # use .open() function and pass in the file path, and "r" for readmode to read our file
+
+
+with open(filepath, 'r') as textFile:
+
+    # store all text into a variable named lines
+    lines = textFile.read()
+
+    print(lines)
